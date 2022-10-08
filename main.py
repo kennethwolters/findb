2cc3a300cb33bb5d5e6f585a70bf65c028e11313
kennethwolters <kenneth@aberin-wolters.net>
2022-10-08 10:22:17 +0200

    slightly better

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-#Dependency
+
+# Dependency
 def get_db():
     db = SessionLocal()
     try:
@@ -16,12 +17,14 @@ def get_db():
     finally:
         db.close()
 
-@app.post("'/users/", response_model=schemas.User)
+
+@app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email already registeres")
+        raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
+
 
 @app.get("/users/", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
--- schemas.py
+++ schemas.py
@@ -1,31 +1,37 @@
-from typing import Union
+from typing import List, Union
 
 from pydantic import BaseModel
+
 
 class ItemBase(BaseModel):
     title: str
     description: Union[str, None] = None
 
+
 class ItemCreate(ItemBase):
     pass
+
 
 class Item(ItemBase):
     id: int
     owner_id: int
 
-    class Cconfig:
+    class Config:
         orm_mode = True
+
 
 class UserBase(BaseModel):
     email: str
 
+
 class UserCreate(UserBase):
     password: str
+
 
 class User(UserBase):
     id: int
     is_active: bool
-    items: list[Item] = []
+    items: List[Item] = []
 
     class Config:
         orm_mode = True
